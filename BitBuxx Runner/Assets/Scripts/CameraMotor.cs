ccd7c5adf7fb185a8b5d428ac3620ed18736c526
Devon Hood <devon_hood@bloomfield.edu>
2022-08-19 04:22:21 -0400

    Fixed Camera Position and Player Start Position

--- BitBuxx Runner/Assets/Scripts/CameraMotor.cs
+++ BitBuxx Runner/Assets/Scripts/CameraMotor.cs
@@ -16,7 +16,6 @@ public class CameraMotor : MonoBehaviour
             return; 
 
         Vector3 desiredPosition = lookAt.position + offset;
-        desiredPosition.x = 0;
         transform.position = Vector3.Lerp(transform.position,desiredPosition,0.1f);
         transform.rotation = Quaternion.Lerp(transform.rotation,Quaternion.Euler(rotation), 0.1f); 
     }
